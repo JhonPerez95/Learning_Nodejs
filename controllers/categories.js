bc00a1c40c2e4ac3e2d0436bc99bd88368a218aa
JhonPerez95 <jhonperez.j95@gmail.com>
2019-10-13 01:07:36 -0500

    add second router categories

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,11 +1,22 @@
 const Category = require("../models/categories");
 
 getAllCategories = (req, res)=>{
-    Category.findAll({ attributes: ['name']})
+    Category.findAll({ attributes: ['name', 'base_category']})
 	.then(categories => res.send(JSON.stringify(categories)))
     .catch(err => res.send(err))
 };
 
+getCategoryId = (req, res)=>{
+    const {id} = req.params;
+    Category.findAll({
+        where:{id_categories: id},
+        attributes: ['name', 'base_category', 'id_user']
+    })
+    .then(categories => res.send(JSON.stringify(categories)))
+    .catch(err => res.send(err))
+}
+
 module.exports = {
-    getAllCategories
+    getAllCategories,
+    getCategoryId
 }
--- routes/categories.js
+++ routes/categories.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const ViewCategories = require("./../controllers/categories");
 
 router.get("/categories/all", ViewCategories.getAllCategories);
+router.get("/categories/all/:id",ViewCategories.getCategoryId);
 
 module.exports = router;
